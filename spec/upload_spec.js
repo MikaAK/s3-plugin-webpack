08a65baaede1b02a091a3cc741e12c594ad73e7e
MikaAK <mikakalathil@gmail.com>
2015-11-12 15:37:42 -0800

    Update babel and create setup for tests

---
+++ spec/upload_spec.js
@@ -0,0 +1,8 @@
+import testHelpers from './upload_test_helpers'
+
+describe('S3 Webpack Upload', function() {
+  it('uploads entire directory to s3 if directory is defined')
+  it('uploads build to s3 if no directory option')
+  it('doesn\'t upload excluded')
+  it('cdnizes links inside of html files')
+})
