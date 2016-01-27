475e8be0b42f100a838cf07971c548c224c3e8cd
MikaAK <mikakalathil@gmail.com>
2016-01-27 10:08:42 -0800

    build dist

--- dist/s3_plugin.js
+++ dist/s3_plugin.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 //import ProgressBar from 'progress'
 
 var _http = require('http');
@@ -60,7 +60,7 @@ var PATH_SEP = _path2.default.sep;
 
 var S3_PATH_SEP = '/';
 
-module.exports = function () {
+module.exports = (function () {
   function S3Plugin() {
     var options = arguments.length <= 0 || arguments[0] === undefined ? {} : arguments[0];
 
@@ -410,4 +410,4 @@ module.exports = function () {
   }]);
 
   return S3Plugin;
-}();
+})();
