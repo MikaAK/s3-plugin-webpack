7a4f49a6b61d9b7bb18e8a5f815853213dcc24c4
Anton <antonbavykin1991@gmail.com>
2017-04-01 01:58:38 +0300

    fixed noname folders in bucket (#68)
    * fixed noname folders in bucket
    
    * fixed const issue
    
    * fixed eslint

--- src/s3_plugin.js
+++ src/s3_plugin.js
@@ -269,10 +269,15 @@ module.exports = class S3Plugin {
   }
 
   uploadFile(fileName, file) {
-    const Key = this.options.basePath + fileName
+    let Key = this.options.basePath + fileName
     const s3Params = _.mapValues(this.uploadOptions, (optionConfig) => {
       return _.isFunction(optionConfig) ? optionConfig(fileName, file) : optionConfig
     })
+
+    // avoid noname folders in bucket
+    if (Key[0] === '/') {
+      Key = Key.substr(1)
+    }
 
     // Remove Gzip from encoding if ico
     if (/\.ico/.test(fileName) && s3Params.ContentEncoding === 'gzip')
